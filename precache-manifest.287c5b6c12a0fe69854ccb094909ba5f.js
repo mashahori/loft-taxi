f711778b3df9554159934ea31d5376ca085e0362
Мария <tmsdesign@yandex.ru>
2020-03-23 14:26:50 +0300

    Updates

---
+++ precache-manifest.287c5b6c12a0fe69854ccb094909ba5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a454745486a2a8ac44ced0025a3ed6",
+    "url": "/loft-taxi/index.html"
+  },
+  {
+    "revision": "4bed4f31588f65188ffd",
+    "url": "/loft-taxi/static/css/main.344d9a48.chunk.css"
+  },
+  {
+    "revision": "b5e885c4e31c493b8ca6",
+    "url": "/loft-taxi/static/js/2.a4b8ee0b.chunk.js"
+  },
+  {
+    "revision": "fddf09485f4567d1741acbb8fd3ac3aa",
+    "url": "/loft-taxi/static/js/2.a4b8ee0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bed4f31588f65188ffd",
+    "url": "/loft-taxi/static/js/main.e87c9af7.chunk.js"
+  },
+  {
+    "revision": "2f2451801595e9e6a394",
+    "url": "/loft-taxi/static/js/runtime-main.f16ff31a.js"
+  },
+  {
+    "revision": "c0bb546cb4376b0459d0e28a056abb82",
+    "url": "/loft-taxi/static/media/login_background.c0bb546c.png"
+  }
+]);
